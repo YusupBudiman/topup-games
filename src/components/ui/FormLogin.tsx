7070c3cf6e0552e480fd73e1991ee5476f2d028d
YusupBudiman <yusupbudiman3@gmail.com>
2026-02-26 15:34:08 +0700

    feat:add ui/ux login user

--- src/components/ui/FormLogin.tsx
+++ src/components/ui/FormLogin.tsx
@@ -3,30 +3,15 @@
 import { useForm } from "react-hook-form";
 import BtnSubmit from "./BtnSubmit";
 export default function FormLogin() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="w-full flex flex-col gap-2"
-    >
+    <form className="w-full flex flex-col gap-2">
       <label htmlFor="email">Email</label>
       <input
         className="bg-transparent border border-gray-700 rounded-md p-2"
-        {...register("email", { required: "Email is required" })}
         placeholder="Email"
       />
-      {errors.email && <p>{errors.email.message}</p>}
 
-      <BtnSubmit onClick={handleSubmit(onSubmit)} type="submit">
+      <BtnSubmit onClick={() => console.log("clicked")} type="submit">
         Lanjutkan
       </BtnSubmit>
     </form>
